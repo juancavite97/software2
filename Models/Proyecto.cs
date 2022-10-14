eccaabad052f2bb4db9e0b599d7ea212f05b9180
juancavite97 <juan_vite@usmp.pe>
2022-10-13 22:16:40 -0500

    prueba

---
+++ Models/Proyecto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftwareII.Models
+{
+        public class Proyecto
+        {
+            public int Id { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public string Description { get; set; }
+            [Required]
+            public int AreaDesarollo { get; set; }
+            [Required]
+            public int Stakeholder { get; set; }
+            [Required]
+            public int AreaQA { get; set; }
+            [Required]
+            public string Code { get; set; }
+            [Required]
+            public int Status { get; set; }
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime FechaInicio { get; set; }
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime FechaFin { get; set; }
+            [Required]
+            public int Rol { get; set; }
+            
+        }
+        
+
+}
+
+
---
+++ Models/Stakeholder.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftwareII.Models
+{
+    public class Stakeholder
+    {
+            public int Id { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public string LastName { get; set; }
+            [Required]
+            public string Company { get; set; }     
+            [RegularExpression(@"^\d{9}$", ErrorMessage ="Only numbers accepted")]
+            [Required]
+            [StringLength(9)]
+            public string Code { get; set; }
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+        
+    }
+}
