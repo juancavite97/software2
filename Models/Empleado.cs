6941218edd87a06b77049887845bec84e0388c12
juancavite97 <juan_vite@usmp.pe>
2022-10-13 22:22:15 -0500

    subiendo

---
+++ Models/Empleado.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftwareII.Models
+{
+    public class Empleado
+    {
+            public int Id { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public string LastName { get; set; }
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+            [Required]
+            public string Area { get; set; }
+            [RegularExpression(@"^\d{9}$", ErrorMessage ="Only numbers accepted")]
+            [Required]
+            [StringLength(9)]
+            public string Code { get; set; }
+            [RegularExpression(@"^\d{9}$", ErrorMessage ="Only numbers accepted")]
+            [Required]
+            [StringLength(9)]
+            [Phone]
+            public string Phone { get; set; }
+            [Required]
+            public string Rol { get; set; }
+    }
+}
+
+
+
